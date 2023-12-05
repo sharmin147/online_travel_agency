7fd83b4a0e7e35e8bb6fac00b594740edb9b8fb0
mojahidjouel <mojahidjouel95@gmail.com>
2023-12-05 12:33:11 +0600

    booking

--- resources/views/frontenduser/booking/create.blade.php
+++ resources/views/frontenduser/booking/create.blade.php
@@ -15,25 +15,33 @@
                             <div class="row">
                               <div class="col-sm-4 col-12">
                                 <div class="form-group">
-                                  <input type="text" class="form-control p-2" placeholder="Your name" required="required" id="name" name="name"  value="{{old('name')}}"/>
-                                  @if($errors->has('name'))
-                                    <small class="d-block text-danger">{{$errors->first('name')}}</small>
-                                  @endif
-                                </div>
+                                    <select class="form-control p-2" id="from" name="from" required="required">
+                                    <option value="">From</option>
+                                    <option value="1" {{ old('from') == '1' ? 'selected' : '' }}>Option 1</option>
+                                    <option value="2" {{ old('from') == '2' ? 'selected' : '' }}>Option 2</option>
+                                    </select>
+                                     @if($errors->has('from'))
+                                       <small class="d-block text-danger">{{$errors->first('from')}}</small>
+                                      @endif
+                                  </div>
                               </div>
                               <div class="col-sm-4 col-12">
                                 <div class="form-group">
-                                  <input type="text" class="form-control p-2" placeholder="Your name" required="required" id="name" name="name"  value="{{old('name')}}"/>
-                                  @if($errors->has('name'))
-                                    <small class="d-block text-danger">{{$errors->first('name')}}</small>
-                                  @endif
-                                </div>
+                                    <select class="form-control p-2" id="to" name="to" required="required">
+                                    <option value="">To</option>
+                                    <option value="1" {{ old('to') == '1' ? 'selected' : '' }}>Option 1</option>
+                                    <option value="2" {{ old('to') == '2' ? 'selected' : '' }}>Option 2</option>
+                                    </select>
+                                     @if($errors->has('to'))
+                                       <small class="d-block text-danger">{{$errors->first('to')}}</small>
+                                      @endif
+                                  </div>
                               </div>
                               <div class="col-sm-4 col-12">
                                 <div class="form-group">
-                                  <input type="text" class="form-control p-2" placeholder="Your name" required="required" id="name" name="name"  value="{{old('name')}}"/>
-                                  @if($errors->has('name'))
-                                    <small class="d-block text-danger">{{$errors->first('name')}}</small>
+                                  <input type="date" class="form-control p-2" placeholder="travel date" required="required" id="travel_date" name="travel_date"  value="{{old('travel_date')}}"/>
+                                  @if($errors->has('travel_date'))
+                                    <small class="d-block text-danger">{{$errors->first('travel_date')}}</small>
                                   @endif
                                 </div>
                               </div>
